d0f50cb1fa175bab0712771a02f0dd77cd281765
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2011-11-05 15:40:35 +0000

    Test class to verify configuration xml

---
+++ src/main/test/java/testng/com/ingby/socbox/bischeck/ConfigurationManagerTest.java
@@ -0,0 +1,24 @@
+package testng.com.ingby.socbox.bischeck;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.ingby.socbox.bischeck.ConfigurationManager;
+
+public class ConfigurationManagerTest {
+	ConfigurationManager confMgmr;
+	@BeforeTest
+    public void beforeTest() throws Exception {
+            //System.setProperty("bishome",".");
+            ConfigurationManager.initonce();
+    		confMgmr = ConfigurationManager.getInstance();
+    		
+    }
+    
+    @Test
+    public void verify_basicxml_return0() {
+            Assert.assertEquals(confMgmr.verify(), 0);
+    }
+    
+}
