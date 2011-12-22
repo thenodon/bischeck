e367e3234002fdeeeb4b686311ddc556838c0e74
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2011-12-22 22:18:34 +0000

    Support for service and service items to connect to a MK livestatus enabled Nagios service over tcp.

---
+++ src/main/java/com/ingby/socbox/bischeck/QueryNagiosPerfData.java
@@ -0,0 +1,105 @@
+/*
+#
+# Copyright (C) 2010-2011 Anders Håål, Ingenjorsbyn AB
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+*/
+
+
+package com.ingby.socbox.bischeck;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.log4j.Logger;
+
+/**
+ * The QueryNagiosPerfData can parse a date specification in a Nagios 
+ * performance data string. The performance data specification must
+ * be in a format:<br>
+ * 'label'=value[UOM];[warn];[crit];[min];[max] 
+ * <br>
+ * @author andersh
+ *
+ */
+public class QueryNagiosPerfData {
+
+	public static void main(String[] args) {
+		System.out.println(QueryNagiosPerfData.parse("load5","load1=0.160;15.000;30.000;0; load5=0.090;10.000;25.000;0; load15=0.020;5.000;20.000;0;"));
+		System.out.println(QueryNagiosPerfData.parse("rta","'rta'=0.251ms;100.000;500.000;0; 'pl'=0%;20;60;;"));
+		System.out.println(QueryNagiosPerfData.parse("size","time=0.009737s;;;0.000000 size=481B;;;0"));
+		System.out.println(QueryNagiosPerfData.parse("dns","'dns'=10ms 'pl'=0%;20;60;;"));
+		
+	}
+	
+	static Logger  logger = Logger.getLogger(QueryNagiosPerfData.class);
+
+	//Find label entry from start of line or space until =
+	static Pattern labelMatch = Pattern.compile("(^| )(.*?)=");
+	//Find data from = to ; or space or end of line
+	static Pattern dataMatch = Pattern.compile("=(.*?)(;|$)");
+	
+	
+	public static String parse(String label, String strtoparse) {
+		logger.debug("Perf data to parse - " + strtoparse);
+		
+
+		Matcher mat = labelMatch.matcher(strtoparse);
+
+		String perflabel = null;
+		boolean labelfound = false;
+		while (mat.find()) {			
+			perflabel = mat.group().trim();
+			logger.debug("<"+perflabel+">");
+			if (perflabel.equals(label+"=") || perflabel.equals("'"+label+"'"+"=")) {
+				labelfound=true; 
+				break;
+			}
+		}//while
+		
+		// Not hits
+		if (!labelfound) return null;
+		
+		Pattern perfMatch = Pattern.compile(perflabel+"(.*?)( |$)");
+		
+		mat = perfMatch.matcher(strtoparse);
+
+		String perfdata = null;
+		while (mat.find()) {
+			perfdata = mat.group();
+			logger.debug(">"+perfdata+"<");		
+		}
+		
+		mat = dataMatch.matcher(perfdata);
+		String data = null;
+		while (mat.find()) {
+			data = removeUOM(mat.group().replaceAll("=","").replaceAll(";", ""));
+			logger.debug("<>"+data+"<>");		
+		}
+		
+		return data;
+	}
+	
+	private static String removeUOM(String s) {
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < s.length(); i++) {
+		   char ch = s.charAt(i);
+		   if (Character.isDigit(ch) || ch == ',' || ch == '.') {
+		     sb.append(ch);
+		   }
+		}
+		return sb.toString();
+	}
+}
---
+++ src/main/java/com/ingby/socbox/bischeck/service/LivestatusService.java
@@ -0,0 +1,130 @@
+/*
+#
+# Copyright (C) 2010-2011 Anders Håål, Ingenjorsbyn AB
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+*/
+
+package com.ingby.socbox.bischeck.service;
+
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.net.URI;
+
+import org.apache.log4j.Logger;
+
+import com.ingby.socbox.bischeck.ConfigurationManager;
+
+
+public class LivestatusService extends ServiceAbstract implements Service {
+
+	static Logger  logger = Logger.getLogger(LivestatusService.class);
+	
+	static private int querytimeout = 10000; //millisec
+	private Socket clientSocket = null;
+	
+	
+	
+	static {
+		try {
+			querytimeout = Integer.parseInt(ConfigurationManager.getInstance().getProperties().
+					getProperty("LiveStatusService.querytimeout","10")) * 1000;
+		} catch (NumberFormatException ne) {
+			logger.error("Property LiveStatusService.querytimeout is not " + 
+					"set correct to an integer: " +
+					ConfigurationManager.getInstance().getProperties().getProperty(
+					"LiveStatusService.querytimeout"));
+		}
+	}
+
+	
+	public LivestatusService (String serviceName) {
+		this.serviceName = serviceName;
+	}
+
+	
+	@Override
+	public void openConnection() throws Exception {
+		URI url = new URI(this.getConnectionUrl());
+	    // Create socket that is connected to server on specified port
+	    clientSocket = new Socket(url.getHost(), url.getPort());
+	    clientSocket.setSoTimeout(querytimeout);
+	    logger.debug("Connected");
+	}
+
+	
+	@Override
+	public void closeConnection() {
+		try {
+			clientSocket.close();
+		} catch (IOException ignore) {}
+	}
+
+	
+	@Override 
+	public String executeStmt(String exec) throws Exception {
+		
+		/*
+		 * Replace all \n occurance with real newlines 
+		 */
+		String message = exec.replaceAll("\\\\n", "\n");
+		
+		DataOutputStream dataOut = null;
+		BufferedReader bufIn = null;
+		
+		StringBuffer responseBuffer = new StringBuffer();
+		
+		logger.debug("Execute request: " + message);
+		
+		dataOut = new DataOutputStream(clientSocket.getOutputStream());
+	    dataOut.writeBytes(message);
+	    dataOut.flush();
+	    
+	    bufIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+	    String responseLine = null;
+	    try {
+	    	while ((responseLine = bufIn.readLine()) != null) {  
+	    		responseBuffer.append(responseLine);    
+	    	}  
+	    } catch (SocketTimeoutException exptime) {
+	    	logger.error("Livestatus connection timed-out after " + querytimeout + " ms");
+	    	return null;
+	    } finally {	
+	    	try {
+				dataOut.close();
+			} catch (IOException ignore) {}  
+	        dataOut = null;  
+
+	        try {
+				bufIn.close();
+			} catch (IOException ignore) {}  
+	        bufIn = null; 
+	    }
+                
+	    String responseMsg = new String(responseBuffer);
+        
+		logger.debug("Received response: " + responseMsg);
+	    
+		return responseMsg;
+	}
+}
+
+
