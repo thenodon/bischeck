7130cab8de6ea4221c107a21a5bf9b2103f43389
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2012-02-05 22:47:47 +0000

    Fixed the granting so no create user is needed.

--- examples/create_bischeckverify.sql
+++ examples/create_bischeckverify.sql
@@ -1,7 +1,6 @@
-create database bischecktest;
-create user 'bischeck'@'localhost' IDENTIFIED BY 'bischeck';
-grant usage on bischecktest.* to 'bischeck'@'localhost';
-grant all on bischecktest.* to 'bischeck'@'localhost';
+create database if not exists bischecktest;
+grant all on bischecktest.* to 'bischeck'@'localhost' IDENTIFIED BY 'bischeck';
+drop table if exists bischecktest.test;
 create table bischecktest.test (id int NOT NULL AUTO_INCREMENT, value int NOT NULL, createdate date, PRIMARY KEY (id));
 insert into bischecktest.test (value, createdate) values (1000,now());
 insert into bischecktest.test (value, createdate) values (2000,now());
