bb5e18ef587e49b59e7fa1bcba5fc504bd914131
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2012-03-19 06:45:01 +0000

    Add createXMLFile method to test the creation of a configuration xml file. Currently only do a System.out to verify capability.

--- src/main/java/com/ingby/socbox/bischeck/ConfigFileManager.java
+++ src/main/java/com/ingby/socbox/bischeck/ConfigFileManager.java
@@ -1,12 +1,14 @@
 package com.ingby.socbox.bischeck;
 
 import java.io.File;
+import java.io.StringWriter;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
@@ -74,36 +76,7 @@ public class ConfigFileManager {
     }
 	
 
-	
-	/*public Object getXMLConfiguration(XMLCONFIG xmlconf) throws Exception {
-        Object obj = null;
-    
-        obj = getXMLConfig(xmlconf.xml(),
-                xmlconf.xsd(),
-                xmlconf.instance());
-    
-                return obj;
-    }
-*/
-	/**
-	 * The method check if the  
-	 * @param xmlName
-	 * @param xsdName
-	 * @param instanceName
-	 * @return
-	 * @throws Exception
-	 */
-/*	synchronized private Object getXMLConfig(String xmlName, String xsdName, String instanceName) throws Exception {
-        Object xmlobj = null;
 
-        xmlobj = xmlcache.get(xmlName);
-        if (xmlobj == null) {
-            xmlobj = createXMLConfig(xmlName, xsdName, instanceName, xmlobj);
-        }
-        return xmlobj;
-    }
-	*/
-	
 	/**
 	 * Retrieve the xml object for a specific configuration file. 
 	 * @param xmlconf member of the enum {@link ConfigXMLInf.XMLCONFIG}
@@ -176,15 +149,14 @@ public class ConfigFileManager {
         return xmlobj;
     }
 
-	/*
-    private Object createXMLConfig(String xmlName, String xsdName,
-            String instanceName) throws Exception {
-    	Object xmlobj = null;
-    	File configfile = new File(ConfigFileManager.initConfigDir(),xmlName);
+	
+	public void createXMLFile(Object xmlobj, XMLCONFIG xmlconf) throws Exception {
+    	
+    	//File configfile = new File(ConfigFileManager.initConfigDir(),xmlconf.xml());
         JAXBContext jc;
         
         try {
-            jc = JAXBContext.newInstance(instanceName);
+            jc = JAXBContext.newInstance(xmlconf.instance());
         } catch (JAXBException e) {
             logger.error("Could not get JAXB context from class");
             throw new Exception(e.getMessage());
@@ -195,40 +167,42 @@ public class ConfigFileManager {
         
         
         //URL xsdUrl = Thread.currentThread().getContextClassLoader().getResource(xsdName);
-        URL xsdUrl = ConfigurationManager.class.getClassLoader().getResource(xsdName);
+        URL xsdUrl = ConfigFileManager.class.getClassLoader().getResource(xmlconf.xsd());
         if (xsdUrl == null) {
             logger.error("Could not find xsd file " +
-                    xsdName + " in classpath");
+            		xmlconf.xsd() + " in classpath");
             throw new Exception("Could not find xsd file " +
-                    xsdName + " in classpath");
+            		xmlconf.xsd() + " in classpath");
         }
         
         try {
             schema = sf.newSchema(new File(xsdUrl.getFile()));
         } catch (Exception e) {
-            logger.error("Could not vaildate xml file " + xmlName + " with xsd file " +
-                    xsdName + ": " + e.getMessage());
+            logger.error("Could not vaildate xml file " + xmlconf.xml() + " with xsd file " +
+            		xmlconf.xsd() + ": " + e.getMessage());
             throw new Exception(e.getMessage());
         } 
 
-        Unmarshaller u = null;
+        Marshaller u = null;
         try {
-            u = jc.createUnmarshaller();
+            u = jc.createMarshaller();
         } catch (JAXBException e) {
-            logger.error("Could not create an unmarshaller for for context");
+            logger.error("Could not create an marshaller for for context");
             throw new Exception(e);
         }
         u.setSchema(schema);
-
+        
+        StringWriter writer = new StringWriter();
         try {
-            xmlobj =  u.unmarshal(configfile);
+        	
+            u.marshal(xmlobj, writer);
         } catch (JAXBException e) {
-            logger.error("Could not unmarshall the file " + xmlName +":" + e);
+            logger.error("Could not unmarshall the file " + xmlconf.xml() +":" + e);
             throw new Exception(e);
         }
-        xmlcache.put(xmlName, xmlobj);
-        logger.debug("Create new object for xml file " + xmlName + " and store in cache");
-        return xmlobj;
+        //xmlcache.put(xmlconf.xml(), xmlobj);
+        logger.debug("Create new object for xml file " + xmlconf.xml() + " and store in cache");
+        System.out.println(writer);
     }
-*/
+
 }
