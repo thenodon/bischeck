81109e797701dd36e3ec3cbb55c38585c10ba885
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2012-05-11 07:18:23 +0000

    New class to manage cache data parsing to real cache data.

---
+++ src/main/java/com/ingby/socbox/bischeck/LastStatusCacheParse.java
@@ -0,0 +1,93 @@
+/*
+#
+# Copyright (C) 2010-2011 Anders Håål, Ingenjorsbyn AB
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+ */
+
+package com.ingby.socbox.bischeck;
+
+
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+import org.apache.log4j.Logger;
+
+/**
+ * This class manage parsing os cached data by replacing a cache entry name
+ * host-service-serviceitem[X] with the data in the cache. 
+ * 
+ * @author andersh
+ *
+ */
+
+public class LastStatusCacheParse {
+
+	static Logger  logger = Logger.getLogger(LastStatusCacheParse.class);
+
+	public static String parse(String str) {
+		Pattern pat = null;
+
+		try {
+			pat = Pattern.compile (LastStatusCache.getInstance().getHostServiceItemFormat());
+
+		} catch (PatternSyntaxException e) {
+			logger.warn("Regex syntax exception, " + e);
+			throw e;
+		}
+
+		Matcher mat = pat.matcher (str);
+
+		String arraystr="";
+		arraystr = Util.parseParameters(str);
+
+		StringTokenizer st = new StringTokenizer(LastStatusCache.getInstance().getParametersByString(arraystr),",");
+
+		// Indicator to see if any parameters are null since then no calc will be done
+		boolean notANumber = false;
+		ArrayList<String> paramOut = new ArrayList<String>();
+
+		while (st.hasMoreTokens()) {
+			String retvalue = st.nextToken(); 
+
+			if (retvalue.equalsIgnoreCase("null")) { 
+				notANumber= true;
+			}
+
+			paramOut.add(retvalue);
+		}
+
+		if (notANumber) { 
+			logger.debug("One or more of the parameters are null");
+			return null;
+		} else  {
+			StringBuffer sb = new StringBuffer ();
+			mat = pat.matcher (str);
+
+			int i=0;
+			while (mat.find ()) {
+				mat.appendReplacement (sb, paramOut.get(i++));
+			}
+			mat.appendTail (sb);
+
+			return sb.toString();
+			
+		}
+	}
+
+}
