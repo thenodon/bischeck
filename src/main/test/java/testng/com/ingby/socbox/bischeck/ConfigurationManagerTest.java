0f169fc8e8c8fa79a37ad3f32d046f44b11ec6ac
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2012-06-16 20:50:44 +0000

    Use VakidateConfiguration

--- src/main/test/java/testng/com/ingby/socbox/bischeck/ConfigurationManagerTest.java
+++ src/main/test/java/testng/com/ingby/socbox/bischeck/ConfigurationManagerTest.java
@@ -24,20 +24,21 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.ingby.socbox.bischeck.ConfigurationManager;
+import com.ingby.socbox.bischeck.ValidateConfiguration;
 
 public class ConfigurationManagerTest {
 	ConfigurationManager confMgmr;
 	@BeforeTest
     public void beforeTest() throws Exception {
             //System.setProperty("bishome",".");
-            ConfigurationManager.initonce();
-    		confMgmr = ConfigurationManager.getInstance();
+            //ConfigurationManager.initonce();
+    		//confMgmr = ConfigurationManager.getInstance();
     		
     }
     
     @Test (groups = { "ConfigurationManager" })
     public void verify_basicxml_return0() {
-            Assert.assertEquals(confMgmr.verify(), 0);
+            Assert.assertEquals(ValidateConfiguration.verify(), 0);
     }
     
 }
