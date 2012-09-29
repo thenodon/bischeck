3ebce006f0719a967c901aee6e77d47d0f1be232
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2012-09-29 15:07:53 +0000

    New service and service item to run check commands from local shell.

---
+++ src/main/java/com/ingby/socbox/bischeck/serviceitem/CheckCommandServiceItem.java
@@ -0,0 +1,123 @@
+/*
+#
+# Copyright (C) 2010-2011 Anders Håål, Ingenjorsbyn AB
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+*/
+
+package com.ingby.socbox.bischeck.serviceitem;
+
+
+import net.sf.json.JSONObject;
+
+import org.apache.log4j.Logger;
+
+import com.ingby.socbox.bischeck.ConfigurationManager;
+import com.ingby.socbox.bischeck.QueryNagiosPerfData;
+import com.ingby.socbox.bischeck.cache.provider.LastStatusCache;
+import com.ingby.socbox.bischeck.service.JDBCService;
+import com.ingby.socbox.bischeck.service.Service;
+import com.ingby.socbox.bischeck.service.ShellService;
+
+/**  
+ * @author andersh
+ *
+ */
+
+public class CheckCommandServiceItem extends ServiceItemAbstract implements ServiceItem {
+    
+	@SuppressWarnings("unused")
+	private final static Logger LOGGER = Logger.getLogger(CheckCommandServiceItem.class);
+
+	public static void main(String[] args) {
+    	try {
+			ConfigurationManager.init();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+    	Service shell = new ShellService("serviceName");
+        
+        ServiceItem checkcommand = new CheckCommandServiceItem("serviceItemName");
+        checkcommand.setService(shell);
+        
+        checkcommand.setExecution("{\"check\":"+
+        		"\"/usr/lib/nagios/plugins/check_ping -H localhost -w 100.0,80% -c 200.0,90%\","+
+        		"\"label\":"+ 
+        		"\"rta\"}");
+    	
+        try {
+			checkcommand.execute();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Return value:" + checkcommand.getLatestExecuted());
+    
+	    checkcommand.setExecution("{\"check\":"+
+        		"\"/usr/lib/nagios/plugins/check_tcp -H localhost -p 22\","+
+        		"\"label\":"+ 
+        		"\"time\"}");
+    	
+        try {
+			checkcommand.execute();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Return value:" + checkcommand.getLatestExecuted());
+    
+    }
+    public CheckCommandServiceItem(String name) {
+        this.serviceItemName = name;        
+    }
+
+    
+    @Override
+    public void execute() throws Exception {
+        /*
+         * Check the operation type - status
+         *  
+         */
+    	
+        JSONObject jsonStatement = JSONObject.fromObject(this.getExecution());
+        if (!validateExecStatement(jsonStatement)) {
+            throw new Exception("Not a valid check command operation " + jsonStatement.toString());
+        }
+
+        String checkcommand = jsonStatement.getString("check");
+
+    	// Execute the check_command
+        LOGGER.info(checkcommand);
+    	String checkres =  service.executeStmt(checkcommand);
+    	LOGGER.info(checkres);
+    	// Error handling
+    	
+    	String perfres = QueryNagiosPerfData.getPerfdata(checkres);
+    	LOGGER.info(perfres);
+    	String checkvalue = QueryNagiosPerfData.parse(jsonStatement.getString("label"),perfres);
+        
+        setLatestExecuted(checkvalue);    
+    }
+
+    private boolean validateExecStatement(JSONObject jsonStatement) {
+        if (!jsonStatement.containsKey("check"))  return false;
+        
+        if (!jsonStatement.containsKey("label")) return false; 
+            
+        return true;
+    }
+
+}
