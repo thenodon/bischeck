5f4e63390d382e2e549456a77e9cdee7b2cf4c8b
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2012-10-09 11:04:40 +0000

    Update regexp rules

--- src/main/java/com/ingby/socbox/bischeck/ObjectDefinitions.java
+++ src/main/java/com/ingby/socbox/bischeck/ObjectDefinitions.java
@@ -2,11 +2,11 @@ package com.ingby.socbox.bischeck;
 
 public class ObjectDefinitions {
 	
-	private static final String ILLEGAL_OBJECT_NAME_CHARS = "`~!$%^&*|'\"<>?,()=";
+	private static final String ILLEGAL_OBJECT_NAME_CHARS = "`+~!$%^&*|'\"<>?,()=";
 
-	private static final String OBJECT_REGEXP = "[a-zA-Z0-9_.@+]{1}[a-zA-Z0-9 @_.+\\\\-]*";
-	
-	private static final String HOST_SERVICE_ITEM_REGEXP = OBJECT_REGEXP +"-" + 
+	private static final String OBJECT_HOSTNAME ="[a-zA-Z0-9]{1}[a-zA-Z0-9_.\\\\-]*[a-zA-Z0-9_]{1}";
+	private static final String OBJECT_REGEXP = "[a-zA-Z0-9_.]{1}[a-zA-Z0-9 _.@\\\\-]*";
+	private static final String HOST_SERVICE_ITEM_REGEXP = OBJECT_HOSTNAME +"-" +
 		OBJECT_REGEXP + "-" +
 		OBJECT_REGEXP +
 		"\\[.*?\\]";
