665c6e175ab53c49671cfbc96394dc71d35fd915
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2012-10-09 12:27:09 +0000

    Update naming rule

--- src/main/java/com/ingby/socbox/bischeck/ObjectDefinitions.java
+++ src/main/java/com/ingby/socbox/bischeck/ObjectDefinitions.java
@@ -4,8 +4,8 @@ public class ObjectDefinitions {
 	
 	private static final String ILLEGAL_OBJECT_NAME_CHARS = "`+~!$%^&*|'\"<>?,()=";
 
-	private static final String OBJECT_HOSTNAME ="[a-zA-Z0-9]{1}[a-zA-Z0-9_.\\\\-]*[a-zA-Z0-9_]{1}";
-	private static final String OBJECT_REGEXP = "[a-zA-Z0-9_.]{1}[a-zA-Z0-9 _.@\\\\-]*";
+	private static final String OBJECT_HOSTNAME ="[a-zA-Z0-9]{1}[a-zA-Z0-9_.\\\\-]*[a-zA-Z0-9]{1}";
+	private static final String OBJECT_REGEXP = "[a-zA-Z0-9_.@]{1}[a-zA-Z0-9 _.@\\\\-]*[a-zA-Z0-9_.@]{1}";
 	private static final String HOST_SERVICE_ITEM_REGEXP = OBJECT_HOSTNAME +"-" +
 		OBJECT_REGEXP + "-" +
 		OBJECT_REGEXP +
