a07e45b305267cab68c6c39515fc60c6c44d4c16
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2012-10-28 22:05:21 +0000

    The provider.LastStatusCacheParse parse has been moved to the CacheUtil to be general for any cache implementing CacheInf.

--- src/main/java/com/ingby/socbox/bischeck/cache/CacheUtil.java
+++ src/main/java/com/ingby/socbox/bischeck/cache/CacheUtil.java
@@ -18,17 +18,24 @@
 */
 package com.ingby.socbox.bischeck.cache;
 
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 import org.apache.log4j.Logger;
+
+import com.ingby.socbox.bischeck.ObjectDefinitions;
+import com.ingby.socbox.bischeck.Util;
+//import com.ingby.socbox.bischeck.cache.provider.LastStatusCache;
 
 public abstract class CacheUtil {
 
 	static Logger  logger = Logger.getLogger(CacheUtil.class);
-	private static final String FINDINTIMEPATTERN = "^-[0-9]+ *[HMS]{1} *$";
-	private static final String FINDTOFROMTIMEPATTERN = "^-[0-9]+ *[HMS]{1}:-[0-9]+ *[HMS]{1} *$";
-	
+	//private static final String FINDINTIMEPATTERN = "^-[0-9]+ *[HMS]{1} *$";
+	//private static final String FINDTOFROMTIMEPATTERN = "^-[0-9]+ *[HMS]{1}:-[0-9]+ *[HMS]{1} *$";
+	private static final String SEP = ";";	
 	
 	/**
 	 * 
@@ -37,7 +44,7 @@ public abstract class CacheUtil {
 	 */
     public static int calculateByTime(String schedule) {
         //"^[0-9]+ *[HMS]{1} *$" - check for a
-        Pattern pattern = Pattern.compile(FINDINTIMEPATTERN);
+        Pattern pattern = Pattern.compile(ObjectDefinitions.getFindintimepattern());
 
         // Determine if there is an exact match
         Matcher matcher = pattern.matcher(schedule);
@@ -58,7 +65,7 @@ public abstract class CacheUtil {
     
     
     public static boolean isByTime(String schedule) {
-    	Pattern pattern = Pattern.compile(FINDINTIMEPATTERN);
+    	Pattern pattern = Pattern.compile(ObjectDefinitions.getFindintimepattern());
 
     	// Determine if there is an exact match
     	Matcher matcher = pattern.matcher(schedule);
@@ -71,7 +78,7 @@ public abstract class CacheUtil {
 
     
     public static boolean isByFromToTime(String schedule) {
-    	Pattern pattern = Pattern.compile(FINDTOFROMTIMEPATTERN);
+    	Pattern pattern = Pattern.compile(ObjectDefinitions.getFindtofromtimepattern());
 
     	// Determine if there is an exact match
     	Matcher matcher = pattern.matcher(schedule);
@@ -81,5 +88,69 @@ public abstract class CacheUtil {
     		return false;
     	}
     }
+
+    
+    /**
+     * This method manage parsing of cached data by replacing a cache entry name
+     * host-service-serviceitem[X] with the data in the cache. 
+     * @param str the expression including cache entries to replace with data
+     * @return - a string where the cache entries are replaced with data or null
+     * if any of the cache entries was "null" 
+     */
+    public static String parse(String str) {
+		Pattern pat = null;
+		logger.debug("String to cache parse: " + str);
+		try {
+			pat = Pattern.compile (ObjectDefinitions.getHostServiceItemRegexp());
+		} catch (PatternSyntaxException e) {
+			logger.warn("Regex syntax exception, " + e);
+			throw e;
+		}
+
+		Matcher mat = pat.matcher (str);
+
+		String arraystr="";
+		arraystr = Util.parseParameters(str);
+		
+		
+		// If no cache definition present return the orignal string
+		if (arraystr.length() == 0) 
+			return str;
+		
+		// If cache entries in the string parse and replace
+		StringTokenizer st = new StringTokenizer(CacheFactory.getInstance().getParametersByString(arraystr),SEP);
+		
+		// Indicator to see if any parameters are null since then no calc will be done
+		boolean notANumber = false;
+		ArrayList<String> paramOut = new ArrayList<String>();
+
+		while (st.hasMoreTokens()) {
+			String retvalue = st.nextToken(); 
+
+			if (retvalue.matches("(?i).*null*")) {
+				notANumber= true;
+				break;
+			}
+
+			paramOut.add(retvalue);
+		}
+
+		if (notANumber) { 
+			logger.debug("One or more of the parameters are null");
+			return null;
+		} else  {
+			StringBuffer sb = new StringBuffer ();
+			mat = pat.matcher (str);
+
+			int i=0;
+			while (mat.find ()) {
+				mat.appendReplacement (sb, paramOut.get(i++));
+			}
+			mat.appendTail (sb);
+			logger.debug("Parsed string with cache data: " + sb.toString());
+			return sb.toString();
+			
+		}
+	}
 
 }
