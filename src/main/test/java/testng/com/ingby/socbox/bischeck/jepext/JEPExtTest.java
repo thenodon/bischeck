bca7f7db74e2138dadc06e36df9acbc59a747710
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2012-11-17 08:45:57 +0000

    New test for JEP

--- src/main/test/java/testng/com/ingby/socbox/bischeck/jepext/JEPExtTest.java
+++ src/main/test/java/testng/com/ingby/socbox/bischeck/jepext/JEPExtTest.java
@@ -19,55 +19,106 @@
 
 package testng.com.ingby.socbox.bischeck.jepext;
 
-import org.nfunk.jep.JEP;
+import org.nfunk.jep.ParseException;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import com.ingby.socbox.bischeck.jepext.Average;
-import com.ingby.socbox.bischeck.jepext.Max;
-import com.ingby.socbox.bischeck.jepext.Min;
+import com.ingby.socbox.bischeck.ConfigurationManager;
+import com.ingby.socbox.bischeck.jepext.ExecuteJEP;
 
 public class JEPExtTest {
 
-	JEP parser = null;
+	static ExecuteJEP parser = null;
+	private boolean supportNull = false;
 	@BeforeTest
-    public void beforeTest()  {
-		parser = new JEP();        // Create a new parser	
-		parser.addStandardFunctions();
-		parser.addStandardConstants();
-		parser.addFunction("avg", new Average()); // Add the custom function
-		parser.addFunction("max", new Max()); // Add the custom function
-		parser.addFunction("min", new Min()); // Add the custom function
-    }
+    public void beforeTest() throws Exception {
+		ConfigurationManager confMgmr = ConfigurationManager.getInstance();
+		
+		if (confMgmr == null) {
+			System.setProperty("bishome", ".");
+			ConfigurationManager.init();
+			confMgmr = ConfigurationManager.getInstance();
+		}	
+		if (ConfigurationManager.getInstance().getProperties().
+				getProperty("notFullListParse","false").equalsIgnoreCase("true"))
+			supportNull=true;
+
+
+		parser = new ExecuteJEP();        // Create a new parser	
+	}
     
     @Test (groups = { "JEP" })
-    public void verifyExtensions() throws Exception {
-    	String expr = "avg(2,6,4,1,2)";
-    	Assert.assertEquals(calc(parser,expr),new Double(3.0));
+    public void verifyFunctions() throws Exception {
+    	String expr = null;
+    	expr = "avg(1,2,3,4,5)";
+    	Assert.assertEquals(calc(expr),new Float(3.0));
 		expr = "avg(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100)";
-		Assert.assertEquals(calc(parser,expr),new Double(50.5));;
+		Assert.assertEquals(calc(expr),new Float(50.5));
 		expr = "max(2,6,4,1,2)";
-		Assert.assertEquals(calc(parser,expr),new Double(6.0));;
+		Assert.assertEquals(calc(expr),new Float(6.0));
 		expr = "min(2,6,4,1,2)";
-		Assert.assertEquals(calc(parser,expr),new Double(1.0));;
+		Assert.assertEquals(calc(expr),new Float(1.0));
+		expr = "min(2,6,4,1,2)/min(2,6,4,1,2)";
+		Assert.assertEquals(calc(expr),new Float(1.0));
+		
+		expr = "avg(2,6,1,2)";
+		Assert.assertEquals(calc(expr),new Float(2.75));
     	
+		expr = "avg(2,6,1,2) / avg(2,6,1,2)";
+		Assert.assertEquals(calc(expr),new Float(1.0));
+    	
+		expr = "avgNull(null,null,null,4,2)";
+		Assert.assertEquals(calc(expr),new Float(3.0));
+
+		expr = "avgNull(null,null,null,null,null)";
+		Assert.assertNull(calc(expr));
+		
+				
+		if (supportNull) {
+			System.out.println("Null in function tests");
+		
+			expr = "avg(null,null,null,4,2)";	
+			Assert.assertEquals(calc(expr),new Float(3.0));
+			
+			expr = "avg(2,6,null,1,2)";
+			Assert.assertEquals(calc(expr),new Float(2.75));
+			
+			expr = "avg(2,6,null,1,2) / avg(2,6,null,1,2)";
+			Assert.assertEquals(calc(expr),new Float(1.0));
+			
+			expr = "avg(null,2,6,null,null,1,2,null) / sum(null,2,6,null,null,1,2,null) * 100";
+			Assert.assertEquals(calc(expr),new Float(25.0));
+			
+			expr = "avg(null,2,6,null,null,1,2,null) / (sum(null,2,6,null,null,1,2,null) * max(2,6,100,1,2))";
+			Assert.assertEquals(calc(expr),new Float(0.0025));
+			
+			expr = "avg(null,null) / avg(2,6,null,1,2)";
+			Assert.assertNull(calc(expr));
+			
+		}else {
+			System.out.println("Not Null in function tests");
+			expr = "avg(null,null,null,4,2)";
+			Assert.assertNull(calc(expr));
+
+			expr = "avg(2,6,null,1,2)";
+			Assert.assertNull(calc(expr));
+
+			expr = "avg(2,6,null,1,2) / avg(2,6,null,1,2)";
+			Assert.assertNull(calc(expr));
+
+		}
+
     }
     
-    public static Double calc(JEP parser, String expr) {
-		double value;
-		parser.parseExpression(expr);                 // Parse the expression
-		if (parser.hasError()) {
+    public static Float calc(String expr) {
+		Float value = null;
+		try {
+		value = parser.execute(expr);                 // Parse the expression
+		} catch (ParseException pe) {
 			System.out.println("Error while parsing");
-			System.out.println(parser.getErrorInfo());
-			return new Double(-1);
-		}
-		
-		value = parser.getValue();                    // Get the value
-		if (parser.hasError()) {
-			System.out.println("Error during evaluation");
-			System.out.println(parser.getErrorInfo());
-			return new Double(-1);
+			System.out.println(pe.getMessage());
+			return new Float(-1);
 		}
 		
 		return value;
