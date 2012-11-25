2de26d23329f10bed37c9401970217d9f7ffc6a9
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2012-11-25 23:24:03 +0000

    Fixed so null support on data is correctly managed

--- src/main/java/com/ingby/socbox/bischeck/cache/CacheEvaluator.java
+++ src/main/java/com/ingby/socbox/bischeck/cache/CacheEvaluator.java
@@ -8,9 +8,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
+import com.ingby.socbox.bischeck.ConfigurationManager;
 import com.ingby.socbox.bischeck.ObjectDefinitions;
 
 public class CacheEvaluator {
@@ -21,7 +21,8 @@ public class CacheEvaluator {
 	private List<String> cacheEntriesName = null;
 	private List<String> cacheEntriesValue = null;
 	private static final Pattern PATTERN_HOST_SERVICE_SERVICEITEM = Pattern.compile (ObjectDefinitions.getHostServiceItemRegexp());
-    
+
+	static boolean notNullSupport = ConfigurationManager.getInstance().getProperties().getProperty("notFullListParse","false").equalsIgnoreCase("false");
 	/**
 	 * 
 	 * @param statement
@@ -32,8 +33,8 @@ public class CacheEvaluator {
 		cacheeval.parse();
 		return cacheeval.getParsedStatement();
 	}
-	
-	
+
+
 	/**
 	 * 
 	 * @param statement
@@ -42,7 +43,7 @@ public class CacheEvaluator {
 		this.statement = statement;
 	}
 
-	
+
 	/**
 	 * 
 	 * @return
@@ -50,8 +51,8 @@ public class CacheEvaluator {
 	public String getParsedStatement() {
 		return parsedstatement;
 	}
-	
-	
+
+
 	/**
 	 * 
 	 * @return
@@ -59,8 +60,8 @@ public class CacheEvaluator {
 	public List<String> statementValues() {
 		return Collections.unmodifiableList(cacheEntriesValue);
 	}
-	
-	
+
+
 	/**
 	 * 
 	 * @return
@@ -68,39 +69,40 @@ public class CacheEvaluator {
 	public List<String> statementEntries() {
 		return Collections.unmodifiableList(cacheEntriesName);
 	}
-	
-	
+
+
 	/***
 	 * 
 	 */
-    public void parse() {
+	public void parse() {
 		//Pattern pat = null;
 		if (LOGGER.isDebugEnabled())
 			LOGGER.debug("String to cache parse: " + statement);
-		
+
 		Matcher mat = PATTERN_HOST_SERVICE_SERVICEITEM.matcher (statement);
 
 		cacheEntriesName = parseParameters(statement);
-		
-		
+
+
 		// If no cache definition present return the orignal string
 		if (cacheEntriesName.size() == 0) 
 			parsedstatement = statement;
-		
+
 		// If cache entries in the string parse and replace
 		cacheEntriesValue = CacheFactory.getInstance().getValues(cacheEntriesName);
-		
+
 		// Indicator to see if any parameters are null since then no calc will be done
 		boolean notANumber = false;
 		//ArrayList<String> paramOut = new ArrayList<String>();
 
 		Iterator<String> iter = cacheEntriesValue.iterator();
+
 		
 		while (iter.hasNext()) {
 			String retvalue = iter.next();
 			if (LOGGER.isDebugEnabled())
 				LOGGER.debug(">>> retvalue " + retvalue);
-			if (retvalue.matches("(?i).*null*")) {
+			if (notNullSupport  && retvalue.matches("(?i).*null*")) {
 				notANumber= true;
 				break;
 			}
@@ -111,6 +113,7 @@ public class CacheEvaluator {
 				LOGGER.debug("One or more of the parameters are null");
 			parsedstatement = null;
 		} else  {
+
 			StringBuffer sb = new StringBuffer ();
 			mat = PATTERN_HOST_SERVICE_SERVICEITEM.matcher (statement);
 
@@ -125,27 +128,27 @@ public class CacheEvaluator {
 		}
 	}
 
-    
-    /**
-     * 
-     * @param execute
-     * @return
-     * @throws PatternSyntaxException
-     */
+
+	/**
+	 * 
+	 * @param execute
+	 * @return
+	 * @throws PatternSyntaxException
+	 */
 	private static List<String> parseParameters(String execute) throws PatternSyntaxException {
-		
+
 		List<String> cacheNameList = new ArrayList<String>();
-		    
-	    Matcher mat = PATTERN_HOST_SERVICE_SERVICEITEM.matcher (execute);
-	
-	    // empty array to be filled with the cache fields to find
-	    
-	    while (mat.find ()) {
-	        String param = mat.group();
-	        cacheNameList.add(param);
-	           
-	    }	    
-	    return cacheNameList;
+
+		Matcher mat = PATTERN_HOST_SERVICE_SERVICEITEM.matcher (execute);
+
+		// empty array to be filled with the cache fields to find
+
+		while (mat.find ()) {
+			String param = mat.group();
+			cacheNameList.add(param);
+
+		}	    
+		return cacheNameList;
 	}
 
 }
