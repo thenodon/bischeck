bbbb06e820bb24782ddfa934ef2000bfc61fb3a5
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2012-12-14 16:38:51 +0000

    Add support to manage class caching when dynamically loading class. This will secure that loaded classes are not constantly growing when doing reload. Add support for the capability of unregister threshold classes when clearing cache.

--- src/main/java/com/ingby/socbox/bischeck/threshold/ThresholdFactory.java
+++ src/main/java/com/ingby/socbox/bischeck/threshold/ThresholdFactory.java
@@ -15,84 +15,122 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-*/
+ */
 
 package com.ingby.socbox.bischeck.threshold;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
+
+import com.ingby.socbox.bischeck.ClassCache;
 import com.ingby.socbox.bischeck.service.Service;
 import com.ingby.socbox.bischeck.serviceitem.ServiceItem;
 
 public class ThresholdFactory {
 
-    private static HashMap<String,Threshold> cache = new HashMap<String,Threshold>();
+	private static Map<String,Threshold> cache = Collections.synchronizedMap(new HashMap<String,Threshold>());
+	private static List<Class<?>> unregistercache = Collections.synchronizedList(new ArrayList<Class<?>>());
 
-    private final static Logger LOGGER = Logger.getLogger(ThresholdFactory.class);
+	private final static Logger LOGGER = Logger.getLogger(ThresholdFactory.class);
 
-    public static Threshold getCurrent(Service service, ServiceItem serviceItem) 
-    throws Exception, ClassNotFoundException {
-        Threshold current = null;
-        
-        synchronized (cache) {
-            /* check if the threshold exists in cache or evaluate new value */
-            current = cache.get(service.getHost().getHostname()+"-"+service.getServiceName()+"-"+serviceItem.getServiceItemName());
+	private static final String UNREGISTER = "unregister";
 
-            if (current == null) {
-                LOGGER.debug("Threshold for " + 
-                        service.getHost().getHostname() + ":" +
-                        service.getServiceName() + ":" +
-                        serviceItem.getServiceItemName() + ":" +
-                        " are created and added to cache");
-                
-                if (serviceItem.getThresholdClassName() != null) {
-                    try {
-                        try {
-                            // Check if this is a package threshold class with a name 
-                            // without package name
-                            current = (Threshold) Thread.currentThread().getContextClassLoader().
-                            loadClass("com.ingby.socbox.bischeck.threshold." + serviceItem.getThresholdClassName()).newInstance();
-                        } catch (ClassNotFoundException e) {
-                            // Check with the full name
-                            try {
-                                current = (Threshold) Thread.currentThread().getContextClassLoader().
-                                loadClass(serviceItem.getThresholdClassName()).newInstance();
-                            }catch (ClassNotFoundException ee) {
-                                LOGGER.fatal("Service class " + serviceItem.getThresholdClassName() + " not found.");
-                                throw ee;
-                            }
-                        }
-                    } catch (InstantiationException e) {
-                        LOGGER.error("Failed to instance Threshold class " +
-                                serviceItem.getThresholdClassName(), e);
-                    } catch (IllegalAccessException e) {
-                        LOGGER.error("Illegal access to instance Threshold class " +
-                                serviceItem.getThresholdClassName(), e);
-                    }                
-                
-                } else { 
-                    current = new DummyThreshold(); 
-                }         
+	public static Threshold getCurrent(Service service, ServiceItem serviceItem) 
+			throws Exception, ClassNotFoundException {
+		Threshold current = null;
 
-                current.setHostName(service.getHost().getHostname());    
-                current.setServiceName(service.getServiceName());
-                current.setServiceItemName(serviceItem.getServiceItemName());
-                
-                current.init();
+		//synchronized (cache) {
+		/* check if the threshold exists in cache or evaluate new value */
+		current = cache.get(service.getHost().getHostname()+"-"+service.getServiceName()+"-"+serviceItem.getServiceItemName());
 
-                cache.put(service.getHost().getHostname()+"-"+service.getServiceName()+"-"+serviceItem.getServiceItemName(),current);
-            }
-        }
+		if (current == null) {
+			if (LOGGER.isDebugEnabled())
+				LOGGER.debug("Threshold for " + 
+						service.getHost().getHostname() + ":" +
+						service.getServiceName() + ":" +
+						serviceItem.getServiceItemName() + ":" +
+						" are created and added to cache");
 
-        return current;
-    }
+			if (serviceItem.getThresholdClassName() != null) {
+				try {
+					try {
+						// Check if this is a package threshold class with a name 
+						// without package name
+						current = (Threshold) ClassCache.getClassByName("com.ingby.socbox.bischeck.threshold." + serviceItem.getThresholdClassName()).newInstance();
+					} catch (ClassNotFoundException e) {
+						// Check with the full name
+						try {
+							current = (Threshold) ClassCache.getClassByName(serviceItem.getThresholdClassName()).newInstance();
+						}catch (ClassNotFoundException ee) {
+							LOGGER.fatal("Service class " + serviceItem.getThresholdClassName() + " not found.");
+							throw ee;
+						}
+					}
+				} catch (InstantiationException e) {
+					LOGGER.error("Failed to instance Threshold class " +
+							serviceItem.getThresholdClassName(), e);
+				} catch (IllegalAccessException e) {
+					LOGGER.error("Illegal access to instance Threshold class " +
+							serviceItem.getThresholdClassName(), e);
+				}                
 
-    
-    public static void clearCache() {
-        synchronized (cache) {
-            LOGGER.info("Clear threshold cache");
-            cache.clear();
-        }
+			} else { 
+				current = new DummyThreshold(); 
+			}         
 
-    }
+			current.setHostName(service.getHost().getHostname());    
+			current.setServiceName(service.getServiceName());
+			current.setServiceItemName(serviceItem.getServiceItemName());
+
+			current.init();
+
+			cache.put(service.getHost().getHostname()+"-"+service.getServiceName()+"-"+serviceItem.getServiceItemName(),current);
+			if (!unregistercache.contains(current.getClass())) {
+				unregistercache.add(current.getClass());
+			}
+
+		} else {
+			if (LOGGER.isDebugEnabled())
+				LOGGER.debug("Threshold for " + 
+						service.getHost().getHostname() + ":" +
+						service.getServiceName() + ":" +
+						serviceItem.getServiceItemName() + ":" +
+						" in cache");
+		}
+		//}
+
+		return current;
+	}
+
+
+	public static void clearCache() {
+
+		LOGGER.info("Clear threshold cache");
+		cache.clear();
+		for (Class<?> clazz: unregistercache) {
+			Method method = null;
+			try {
+				method = clazz.getMethod(UNREGISTER);				
+				method.invoke(null);
+			} catch (IllegalArgumentException e) {
+				LOGGER.error(e.toString() + ":" + e.getMessage());
+			} catch (IllegalAccessException e) {
+				LOGGER.error(e.toString() + ":" + e.getMessage());
+			} catch (InvocationTargetException e) {
+				LOGGER.error(e.toString() + ":" + e.getMessage());
+			} catch (SecurityException e) {
+				LOGGER.error(e.toString() + ":" + e.getMessage());
+			} catch (NoSuchMethodException e) {
+				LOGGER.error(e.toString() + ":" + e.getMessage());
+			}
+
+		}
+	}
 }
