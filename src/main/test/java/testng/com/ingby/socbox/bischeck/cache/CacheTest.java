ec17046963df1f344548bfca780efd7ec9f88948
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2013-02-03 10:21:33 +0000

    Adding more test for cache time testing.

--- src/main/test/java/testng/com/ingby/socbox/bischeck/cache/CacheTest.java
+++ src/main/test/java/testng/com/ingby/socbox/bischeck/cache/CacheTest.java
@@ -15,11 +15,13 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-*/
+ */
 
 package testng.com.ingby.socbox.bischeck.cache;
 
 
+
+import java.util.Date;
 
 import org.testng.Assert;
 
@@ -35,7 +37,7 @@ import com.ingby.socbox.bischeck.cache.provider.LastStatusCacheParse;
 public class CacheTest {
 
 	ConfigurationManager confMgmr = null;
-	
+
 	String hostname = "test-server.ingby.com";
 	String qhostname = "test\\-server.ingby.com";
 	String servicename = "service@first";
@@ -43,74 +45,144 @@ public class CacheTest {
 	String cachekey = Util.fullName(qhostname, servicename, serviceitemname);
 
 	private boolean supportNull = false;
-	
+
 	@BeforeTest
-    public void beforeTest() throws Exception {
-	
+	public void beforeTest() throws Exception {
+
 		confMgmr = ConfigurationManager.getInstance();
-	
+
 		if (confMgmr == null) {
 			System.setProperty("bishome", ".");
 			ConfigurationManager.init();
 			confMgmr = ConfigurationManager.getInstance();	
 		}
-		
+
 		if (ConfigurationManager.getInstance().getProperties().
 				getProperty("notFullListParse","false").equalsIgnoreCase("true"))
-			supportNull =true;
+			supportNull = true;
 
-		
+
 	}
 
 
 	@Test (groups = { "Cache" })
 	public void verifyCache() {
-			
-		long current = System.currentTimeMillis() - 10*300*1000;
-		
+
+		long current = System.currentTimeMillis() - 20*300*1000;
+
+		((LastStatusCache) LastStatusCache.getInstance()).clearCache();
+
 		for (int i = 1; i < 11; i++) {
 			LastStatus ls = new LastStatus(""+i, (float) i,  current + i*300*1000);
-			System.out.println(CacheTest.class.getName()+">> " + i+":"+ls.getValue() +">"+hostname+"-"+servicename+"-"+serviceitemname);
-			LastStatusCache.getInstance().add(hostname, servicename, serviceitemname, ls.getValue(), ls.getThreshold());
-		
+			System.out.println(CacheTest.class.getName()+">"+(new Date(ls.getTimestamp())).toString() +"> " + i+":"+ls.getValue() +">"+hostname+"-"+servicename+"-"+serviceitemname);
+			LastStatusCache.getInstance().add(ls, Util.fullName( hostname, servicename, serviceitemname));
 		}
-		LastStatus ls = new LastStatus("null", (float) 12,  current + 12*300*1000);
+		LastStatus ls = new LastStatus("null", (float) 11,  current + 12*300*1000);
 		LastStatusCache.getInstance().add(hostname, servicename, serviceitemname, ls.getValue(), ls.getThreshold());
-		
-		for (int i = 1; i < 11; i++) {
+
+		for (int i = 12; i < 22; i++) {
 			ls = new LastStatus(""+i, (float) i,  current + i*300*1000);
-			System.out.println(CacheTest.class.getName()+">> " + i+":"+ls.getValue() +">"+hostname+"-"+servicename+"-"+serviceitemname);
-			LastStatusCache.getInstance().add(hostname, servicename, serviceitemname, ls.getValue(), ls.getThreshold());
-		
+			System.out.println(CacheTest.class.getName()+">"+(new Date(ls.getTimestamp())).toString() +"> " + i+":"+ls.getValue() +">"+hostname+"-"+servicename+"-"+serviceitemname);
+			LastStatusCache.getInstance().add(ls, Util.fullName( hostname, servicename, serviceitemname));
 		}
-	
+
 		if (supportNull) {
 			System.out.println("SUPPORT NULL");
+
+			Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[0]"),"21");
+			Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[9]"),"12");
+			Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[10]"),"null");
 			
-			Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[0]"),"10");
-			Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[9]"),"1");
-			Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[3:6]"),"7,6,5,4");
+			Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[3:6]"),"18,17,16,15");
 			// Test that a limited list will be returned even with some index out of
-			// bounds
-			Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[6:12]"),"4,3,2,1,10,9");
+			// bounds. The real list is "15,14,13,12,null,10,9"
+			Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[6:12]"),"15,14,13,12,10,9");
 			// Test all keys outside range return "null"
 			Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[25:30]"),"null");
+			//Combo 1
+			Assert.assertEquals(LastStatusCacheParse.parse("avg(" + cachekey + "[0]," + cachekey + "[6:12])"),"avg(21,15,14,13,12,10,9)");
+			//Combo 2
+			Assert.assertEquals(LastStatusCacheParse.parse("avg(" + cachekey + "[10]," + cachekey + "[6:12])"),"avg(null,15,14,13,12,10,9)");
+			//Combo 1
+			Assert.assertEquals(LastStatusCacheParse.parse("avg(" + cachekey + "[0]," + cachekey + "[6:12])"),"avg(21,15,14,13,12,10,9)");
+			//Combo 3
+			Assert.assertEquals(LastStatusCacheParse.parse("avg(" + cachekey + "[-5M:-120M]," + cachekey + "[6:12])"),"avg(null,15,14,13,12,10,9)");
+			
 			// Test that a time range with no data in the cache returns "null"
+			Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[-5M:-15M]"),"19,18,17");
+			// Test that a if the end time range with no data in the cache returns "null"
+			Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[-5M:-120M]"),"null");
+
 		} else {
 			System.out.println("DO NOT SUPPORT NULL");
-			
-			Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[0]"),"10");
-			Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[9]"),"1");
-			Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[3:6]"),"7,6,5,4");
+
+			Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[0]"),"21");
+			Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[9]"),"12");
+			Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[3:6]"),"18,17,16,15");
 			// Test that a limited list will be returned even with some index out of
 			// bounds
-			Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[6:12]"),"4,3,2,1,null,10,9");
+			System.out.println("LastStatusCacheParse.parse(cachekey + \"[6:12]\") " +  LastStatusCacheParse.parse(cachekey + "[6:12]"));
+			Assert.assertNull(LastStatusCacheParse.parse(cachekey + "[6:12]"));
 			// Test all keys outside range return "null"
+			System.out.println("LastStatusCacheParse.parse(cachekey + \"[25:30]\") " + LastStatusCacheParse.parse(cachekey + "[25:30]"));
 			Assert.assertNull(LastStatusCacheParse.parse(cachekey + "[25:30]"));
 			// Test that a time range with no data in the cache returns "null"
+			// Test that a time range with no data in the cache returns "null"
+			Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[-5M:-15M]"),"19,18,17");
+			// Test that a if the end time range with no data in the cache returns "null"
+			Assert.assertNull(LastStatusCacheParse.parse(cachekey + "[-5M:-120M]"));
+
 		}
-		
 	}
-	
-	
+
+	@Test (groups = { "Cache" })
+	public void verifyCacheTime() {
+
+		int count = 2000;
+		int secinterval = 300; 
+		long current = System.currentTimeMillis() - count*secinterval*1000;
+
+		((LastStatusCache) LastStatusCache.getInstance()).clearCache();
+
+		for (int i = 1; i < count+1; i++) {
+			LastStatus ls = new LastStatus(""+i, (float) i,  current + i*300*1000);
+			//System.out.println(CacheTest.class.getName()+">"+(new Date(ls.getTimestamp())).toString() +"> " + i+":"+ls.getValue() +">"+hostname+"-"+servicename+"-"+serviceitemname);
+			LastStatusCache.getInstance().add(ls, Util.fullName( hostname, servicename, serviceitemname));
+		}
+
+
+		Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[-300S:-900S]"),"1999,1998,1997");
+		Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[-5M:-15M]"),"1999,1998,1997");
+		Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[-1H:-3H]"),"1988,1987,1986,1985,1984,1983,1982,1981,1980,1979,1978,1977,1976,1975,1974,1973,1972,1971,1970,1969,1968,1967,1966,1965,1964");
+		if (supportNull) {
+			System.out.println("SUPPORT NULL");
+			Assert.assertEquals(LastStatusCacheParse.parse(cachekey + "[-3H:-1H]"),"null");
+		} else {
+			// If from is larger then to => null
+			Assert.assertNull(LastStatusCacheParse.parse(cachekey + "[-3H:-1H]"));	
+		}
+	}
+
+	@Test (groups = { "Cache" })
+	public void superAdd() {
+		int count = 10001;
+		long current = System.currentTimeMillis() - count*300*1000;
+
+		((LastStatusCache) LastStatusCache.getInstance()).clearCache();
+
+		long start = System.currentTimeMillis();
+		for (int i = 1; i < count; i++) {
+			LastStatus ls = new LastStatus(""+i, (float) i,  current + i*300*1000);
+			//System.out.println(">> " + i+":"+ls.getValue() +">"+hostname+"-"+servicename+"-"+serviceitemname);
+			LastStatusCache.getInstance().add(hostname, servicename, serviceitemname, ls.getValue(), ls.getThreshold());
+
+		}
+		long exec = System.currentTimeMillis() - start;
+
+
+
+		System.out.println("Insert " + count + " " + (exec*1000/count) + " us Total time " + (exec) + " msec");
+
+	}
+
 }
