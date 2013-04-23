5b659da21eff29847783085b45fc22d1d9eb93da
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2013-04-23 09:14:14 +0000

    Add the static destroy method that is delegated to the cache implementation. Used to remove the cache completely and save. New init method that that a class name for the cache implementation to use.

--- src/main/java/com/ingby/socbox/bischeck/cache/CacheFactory.java
+++ src/main/java/com/ingby/socbox/bischeck/cache/CacheFactory.java
@@ -19,6 +19,8 @@ public class CacheFactory {
 
 	static CacheInf cache = null;
 
+	private static String classCacheName;
+
 	/**
 	 * Create a cache used by bischeck
 	 * @return
@@ -43,13 +45,21 @@ public class CacheFactory {
 		}
 	}
 
-	@SuppressWarnings("unchecked")
 	public synchronized static void init()  throws CacheException {
+		String className = ConfigurationManager.getInstance().getProperties().
+				getProperty("cacheProvider","com.ingby.socbox.bischeck.cache.provider.redis.LastStatusCache");
+		init(className);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public synchronized static void init(String className)  throws CacheException {
+		classCacheName = className;
 		if (cache == null) {
 			// Selector to define which one to use
+			/*
 			String className = ConfigurationManager.getInstance().getProperties().
 					getProperty("cacheProvider","com.ingby.socbox.bischeck.cache.provider.redis.LastStatusCache");
-
+			*/
 			Class<CacheInf> clazz;
 			try {
 				clazz = (Class<CacheInf>) ClassCache.getClassByName(className);
@@ -72,4 +82,28 @@ public class CacheFactory {
 			LOGGER.info("Cache provider selected is - " + className);
 		}
 	}
+
+	@SuppressWarnings("unchecked")
+	public synchronized static void destroy()  throws CacheException {
+		Class<CacheInf> clazz;
+		try {
+			clazz = (Class<CacheInf>) ClassCache.getClassByName(classCacheName);
+			(clazz.getMethod("destroy")).invoke(null);
+		} catch (ClassNotFoundException e) {
+			throw new CacheException(e);
+		}catch (IllegalArgumentException e) {
+			throw new CacheException(e);
+		} catch (SecurityException e) {
+			throw new CacheException(e);
+		} catch (IllegalAccessException e) {
+			throw new CacheException(e);
+		} catch (InvocationTargetException e) {
+			throw new CacheException(e);
+		} catch (NoSuchMethodException e) {
+			throw new CacheException(e);
+		}
+
+		LOGGER.info("Cache provider destroyed - " + classCacheName);
+	}
 }
+
