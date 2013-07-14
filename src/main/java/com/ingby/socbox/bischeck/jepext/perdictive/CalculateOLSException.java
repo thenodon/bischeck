954b5b7dbaf60812a6f4a6158ed1689042e5f390
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2013-07-14 16:39:43 +0000

    Exception class for the OLS

---
+++ src/main/java/com/ingby/socbox/bischeck/jepext/perdictive/CalculateOLSException.java
@@ -0,0 +1,23 @@
+package com.ingby.socbox.bischeck.jepext.perdictive;
+
+public class CalculateOLSException extends Exception {
+
+	private static final long serialVersionUID = 3361437868565558089L;
+	
+	public CalculateOLSException() {
+		super();
+	}
+
+	public CalculateOLSException(String message) {
+		super(message);
+	}
+
+	public CalculateOLSException(String message,Throwable cause) {
+		super(message, cause);
+	}
+
+	public CalculateOLSException(Throwable cause) {
+		super(cause);
+	}
+
+}
