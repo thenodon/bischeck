99c38c1e3209bf9f7d831e10c6fe03681f41aba7
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2013-08-07 20:54:34 +0000

    The CacheQueue replace LinkedList. Its extend LinkedList and implment a new constructor to set fixed max size of the List and an add that removes the oldest if the size is over the max size.

---
+++ src/main/java/com/ingby/socbox/bischeck/cache/CacheQueue.java
@@ -0,0 +1,32 @@
+package com.ingby.socbox.bischeck.cache;
+
+import java.util.LinkedList;
+
+public class CacheQueue<E> extends LinkedList<E> {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1886983361399250646L;
+	
+	private Integer maxsize;
+
+	public CacheQueue(Integer size) {
+		super();
+		maxsize = size;	
+	}
+	
+	@Override
+	public void addFirst(E ls) {
+		if (maxsize == 0) 
+			return;
+		
+		if (size() >= maxsize) {
+			removeLast();
+		}
+		
+		super.addFirst(ls);
+	}
+	
+	
+}
