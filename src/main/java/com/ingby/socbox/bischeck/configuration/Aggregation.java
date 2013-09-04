1da3a693535151c64f179ab9030c12152bc5750f
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2013-09-04 19:19:02 +0000

    Changed the setAggregateAll to setAggregate

--- src/main/java/com/ingby/socbox/bischeck/configuration/Aggregation.java
+++ src/main/java/com/ingby/socbox/bischeck/configuration/Aggregation.java
@@ -11,105 +11,205 @@ import com.ingby.socbox.bischeck.serviceitem.ServiceItemFactory;
 import com.ingby.socbox.bischeck.xsd.bischeck.XMLAggregate;
 import com.ingby.socbox.bischeck.xsd.bischeck.XMLCache;
 
+/**
+ * The class is used to create configuration for a service definition that is
+ * configured with the aggregate tag.
+ */
 public class Aggregation {
 
-	/**
-	 * The method setup the aggregation definition if 
-	 * @param xmlconfig
-	 * @param service
-	 * @param serviceitem
-	 * @throws Exception
-	 */
-    static void setAggregate(XMLCache xmlconfig, Service service, ServiceItem serviceitem) throws Exception {
-    	if (xmlconfig == null)
-    		return;
-    	
-    	for (XMLAggregate aggregated: xmlconfig.getAggregate()) {
+	private static final String WEEKEND = "\\weekend";
 
-    		Service aggregatedService = null;
-			
-    		if (aggregated.isUseweekend()) {
-    			aggregatedService = ServiceFactory.createService(
-    					service.getServiceName()+ "\\" + aggregated.getPeriod() + "\\" + aggregated.getMethod() + "\\weekend",
-    					"bischeck://cache");
-    		} else {
-    			aggregatedService = ServiceFactory.createService(
-        				service.getServiceName()+ "\\" + aggregated.getPeriod()  + "\\" + aggregated.getMethod(),
-        				"bischeck://cache");
-    		}
-    		
-    		
-    		aggregatedService.setHost(service.getHost());
-    		//service.setAlias(serviceconfig.getAlias());
-    		aggregatedService.setDecscription("");
-    		aggregatedService.setSchedules(getAggregatedSchedule(aggregated));
-    		aggregatedService.setConnectionUrl("bischeck://cache");
-    		aggregatedService.setSendServiceData(false);
-    		
-    		ServiceItem aggregatedServiceItem = null;
-			
-    		aggregatedServiceItem = ServiceItemFactory.createServiceItem(
-    				//serviceitem.getServiceItemName() + "\\" + aggregated.getMethod(),
-    				serviceitem.getServiceItemName(),
-        			"CalculateOnCache");
+	enum AGGREGATION  { 
+		HOUR { 
+			public String toString() {
+				return "HOUR";
+			}
+			public String prefix() {
+				return "H";
+			}
+			public String execStatInclWeekend() {
+				return "[-0H:-1H]";
+			}
+			public String execStat() {
+				return "[-0H:-1H]";
+			}
+			public String execPrefix() {
+				return "";
+			}
+			public String scheduleInclWeekend() {
+				return "0 0 * ? * *";
+			}
+			public String schedule() {
+				return "0 0 * ? * MON-FRI";
+			}
+		}, 
+		DAY { 
+			public String toString() {
+				return "DAY";
+			}
+			public String prefix() {
+				return "D";
+			}
+			public String execStatInclWeekend() {
+				return "[0:24]";
+			}
+			public String execStat() {
+				return "[0:24]";
+			}
+			public String execPrefix() {
+				return "\\H\\";
+			}
+			public String scheduleInclWeekend() {
+				return "0 0 0 ? * *";
+			}
+			public String schedule() {
+				return "0 0 0 ? * MON-FRI";
+			}
+		}, 
+		WEEK { 
+			public String toString() {
+				return "WEEK";
+			}
+			public String prefix() {
+				return "W";
+			}
+			public String execStatInclWeekend() {
+				return "[0:7]";
+			}
+			public String execStat() {
+				return "[0:5]";
+			}
+			public String execPrefix() {
+				return "\\D\\";
+			}
+			public String scheduleInclWeekend() {
+				return "0 0 0 ? * MON";
+			}
+			public String schedule() {
+				return "0 0 0 ? * FRI";
+			}
+		}, 
 
-    		aggregatedServiceItem.setClassName("CalculateonCache");
-    		aggregatedServiceItem.setExecution(getAggregatedExecution(aggregated,service,serviceitem));
-    		
-    		
-    		aggregatedServiceItem.setService(aggregatedService);
-    		aggregatedService.addServiceItem(aggregatedServiceItem);
-    		service.getHost().addService(aggregatedService);
-    		
-    	}
-    }
-    
-    
-    static private String getAggregatedExecution(XMLAggregate aggregated,
+		MONTH {         	 
+			public String toString() {
+				return "MONTH";
+			}
+			public String prefix() {
+				return "M";
+			}
+			public String execStatInclWeekend() {
+				return "[0:4]";
+			}
+			public String execStat() {
+				return "[0:4]";
+			}
+			public String execPrefix() {
+				return "\\W\\";
+			}
+			public String scheduleInclWeekend() {
+				return "0 0 0 ? * MON";
+			}
+			public String schedule() {
+				return "0 0 0 L * ?";
+			}
+		};
+
+		public abstract String prefix();
+		public abstract String execStat();
+		public abstract String execStatInclWeekend();
+		public abstract String execPrefix();
+		public abstract String schedule();
+		public abstract String scheduleInclWeekend();
+
+	}
+
+
+	static void setAggregate(XMLCache xmlconfig, Service service, ServiceItem serviceitem) throws Exception {
+		if (xmlconfig == null)
+			return;
+
+		ArrayList<AGGREGATION> periods = new ArrayList<AGGREGATION>();
+		periods.add(AGGREGATION.HOUR);
+		periods.add(AGGREGATION.DAY);
+		periods.add(AGGREGATION.WEEK);
+		periods.add(AGGREGATION.MONTH);
+
+		for (AGGREGATION period:periods) {
+			for (XMLAggregate aggregated: xmlconfig.getAggregate()) {
+
+				Service aggregatedService = null;
+
+				if (aggregated.isUseweekend()) {
+					aggregatedService = ServiceFactory.createService(
+							service.getServiceName()+ "\\" + period.prefix() + "\\" + aggregated.getMethod() + "\\weekend",
+							"bischeck://cache");
+				} else {
+					aggregatedService = ServiceFactory.createService(
+							service.getServiceName()+ "\\" + period.prefix()  + "\\" + aggregated.getMethod(),
+							"bischeck://cache");
+				}
+
+
+				aggregatedService.setHost(service.getHost());
+				aggregatedService.setDecscription("");
+				aggregatedService.setSchedules(getAggregatedSchedule(period,aggregated.isUseweekend()));
+				aggregatedService.setConnectionUrl("bischeck://cache");
+				aggregatedService.setSendServiceData(false);
+
+				ServiceItem aggregatedServiceItem = null;
+
+				aggregatedServiceItem = ServiceItemFactory.createServiceItem(
+						serviceitem.getServiceItemName(),
+						"CalculateOnCache");
+
+				aggregatedServiceItem.setClassName("CalculateonCache");
+				aggregatedServiceItem.setExecution(getAggregatedExecution(period, aggregated,service,serviceitem));
+
+
+				aggregatedServiceItem.setService(aggregatedService);
+				aggregatedService.addServiceItem(aggregatedServiceItem);
+				service.getHost().addService(aggregatedService);
+
+			}
+		}
+	}
+
+
+	static private String getAggregatedExecution(AGGREGATION agg, XMLAggregate aggregated,
 			Service service, ServiceItem serviceitem) {
+		String aggregatedExecStatement = null;	
 
-    	String aggregatedExecStatement = null;
-    	
-    	if (aggregated.getPeriod().equals("H")) {		
-    		aggregatedExecStatement = aggregated.getMethod() + "(" + Util.fullQoutedName(service, serviceitem) + "[-0H:-1H]" + ")";
-    	} else if (aggregated.getPeriod().equals("D")) {
-    		aggregatedExecStatement = aggregated.getMethod() + "(" + Util.fullQoutedName(service, serviceitem) + "[-0H:-24H]" + ")";
-    	} else if (aggregated.getPeriod().equals("W")) {
-    		if (aggregated.isUseweekend()) {
-    			aggregatedExecStatement = aggregated.getMethod() + "(" + Util.fullQoutedName(service, serviceitem) + "[-0D:-7D]" + ")";
-    		} else {
-    			aggregatedExecStatement = aggregated.getMethod() + "(" + Util.fullQoutedName(service, serviceitem) + "[-0D:-5D]"  + ")";
-    		}
-    	}
-    	return aggregatedExecStatement;
-		
+		if (agg.toString().equals("HOUR")) {
+			aggregatedExecStatement = aggregated.getMethod() + "(" + Util.fullQoutedName(service, serviceitem) + 
+					agg.execStatInclWeekend() +")";
+		} else {
+
+			if (aggregated.isUseweekend()) {
+				aggregatedExecStatement = aggregated.getMethod() + "(" + Util.fullQoutedName(service, serviceitem, 
+						agg.execPrefix()+ aggregated.getMethod() + WEEKEND) +
+						agg.execStatInclWeekend() + 
+						")";
+
+			} else {
+				aggregatedExecStatement = aggregated.getMethod() + "(" + Util.fullQoutedName(service, serviceitem, 
+						agg.execPrefix()+ aggregated.getMethod()) + 
+						agg.execStat()+ ")";
+			}	
+		}
+		return aggregatedExecStatement;
 	}
 
 
-	static private List<String> getAggregatedSchedule(XMLAggregate aggregated) {
-    	List<String> schedules = new ArrayList<String>();
-    	
-    	if (aggregated.getPeriod().equals("H")) {
-    		if (aggregated.isUseweekend()) {
-    			schedules.add("0 */1 * ? * *");
-    		} else {
-    			schedules.add("0 0 * ? * MON-FRI");
-    		}
-    	} else if (aggregated.getPeriod().equals("D")) {
-    		if (aggregated.isUseweekend()) {
-    			schedules.add("0 0 0 ? * *");
-    		} else {
-    			schedules.add("0 0 0 ? * MON-FRI");
-    		}
-    	} else if (aggregated.getPeriod().equals("W")) {
-    		if (aggregated.isUseweekend()) {
-    			schedules.add("0 0 0 ? MON *");
-    		} else {
-    			schedules.add("0 0 0 ? * FRI");
-    		}
-    	}
-    	return schedules;
-	}
+	static private List<String> getAggregatedSchedule(AGGREGATION agg, Boolean useWeekend) {
+		List<String> schedules = new ArrayList<String>();
 
+		if (useWeekend) {
+			schedules.add(agg.scheduleInclWeekend());
+		} else {
+			schedules.add(agg.schedule());
+		}
+
+		return schedules;
+	}
 
 }
