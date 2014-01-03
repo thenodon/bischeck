fc3f74df72ffd5f765a14bfbf130befdb1a0fc85
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2014-01-03 21:25:08 +0000

    The interface used for all server workers

---
+++ src/main/java/com/ingby/socbox/bischeck/servers/WorkerInf.java
@@ -0,0 +1,35 @@
+/*
+#
+# Copyright (C) 2010-2013 Anders Håål, Ingenjorsbyn AB
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+*/
+package com.ingby.socbox.bischeck.servers;
+
+import com.ingby.socbox.bischeck.service.Service;
+
+public interface WorkerInf {
+    
+    
+    /**
+     * Send the Service information to the server. Implementation is responsible
+     * to manage protocol and formatting of message data.
+     * @param service
+     * @throws ServerException if any communication exception occur. 
+     * If {@ ServerCircuitBreak} is used it will be triggered by the exception.
+     */
+    public void send(Service service) throws ServerException;
+
+}
