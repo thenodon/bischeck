0512fc8a4f64ddd704d735e4ea1a9537af25cd63
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2014-08-15 04:44:49 +0000

    Refactored to try-with-resources

--- src/main/java/com/ingby/socbox/bischeck/configuration/DocManager.java
+++ src/main/java/com/ingby/socbox/bischeck/configuration/DocManager.java
@@ -346,30 +346,15 @@ public class DocManager implements ConfigXMLInf {
                     + fromFileName);
         }
         
-        FileInputStream from = null;
-        FileOutputStream to = null;
-        try {
-            from = new FileInputStream(fromFile);
-            to = new FileOutputStream(toFile);
+        try (FileInputStream from = new FileInputStream(fromFile);
+        	FileOutputStream to =  new FileOutputStream(toFile);
+        	){
             byte[] buffer = new byte[4096];
             int bytesRead;
 
             while ((bytesRead = from.read(buffer)) != -1) {
                 to.write(buffer, 0, bytesRead); 
             }
-        } finally {
-        	if (from != null) { 
-        		try {
-        			from.close();
-        		} catch (IOException ignore) {
-        		}
-        	}
-        	if (to != null) {
-        		try {
-        			to.close();
-        		} catch (IOException ignore) {
-        		}
-        	}
         }
     }
 }
