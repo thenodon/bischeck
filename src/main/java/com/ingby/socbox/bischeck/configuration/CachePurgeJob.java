b9be103490c44271d0a650375aea52b34de13519
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2014-09-10 16:55:24 +0000

    [TR-259] - "Using period rather than count for cache purge throws error" This was a NullPointerException thrown if index was null. Index was null if there was no item in the cache that was older then the purge period.

--- src/main/java/com/ingby/socbox/bischeck/configuration/CachePurgeJob.java
+++ src/main/java/com/ingby/socbox/bischeck/configuration/CachePurgeJob.java
@@ -137,7 +137,10 @@ public class CachePurgeJob implements Job {
 								servicedef.getServiceName(), 
 								servicedef.getServiceItemName(),
 								System.currentTimeMillis() + ((long) CacheUtil.calculateByTime(purgeMap.get(key)))*1000);
-						((CachePurgeInf) cache).trim(key, index);
+						// if index is null there is no items in the cache older then the time offset
+						 if (index != null) {
+							 ((CachePurgeInf) cache).trim(key, index);
+						 } 
 					} else {
 						((CachePurgeInf) cache).trim(key, Long.valueOf(purgeMap.get(key)));
 					}
