48fda74cb049ead15b06ce771fafb53bb1a07f59
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2014-10-27 12:36:28 +0000

    Minor refactoring

--- src/main/java/com/ingby/socbox/bischeck/servers/ServerExecutor.java
+++ src/main/java/com/ingby/socbox/bischeck/servers/ServerExecutor.java
@@ -31,29 +31,29 @@ import org.slf4j.LoggerFactory;
 import com.codahale.metrics.Timer;
 import com.ingby.socbox.bischeck.configuration.ConfigurationManager;
 import com.ingby.socbox.bischeck.monitoring.MetricsManager;
-import com.ingby.socbox.bischeck.service.Service;
 import com.ingby.socbox.bischeck.service.ServiceTO;
 import com.ingby.socbox.bischeck.threshold.Threshold.NAGIOSSTAT;
 
 /**
  * 
  * @see {@link ServerMessageExecutor}
- *
+ * 
  */
 
 @Deprecated
 public final class ServerExecutor {
 
-    private final static Logger LOGGER = LoggerFactory.getLogger(ServerExecutor.class);
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(ServerExecutor.class);
 
-    private static ServerExecutor serverexecutor= null;
+    private static ServerExecutor serverexecutor = null;
     /**
-     * The serverSet holds all server configuration from servers.xml where 
-     * the key is the name of the configuration, server name="NSCA">, the 
-     * value is the class. The class must implement the Server class. 
-     * The serverSet is provided through the ConfigurationManager.
+     * The serverSet holds all server configuration from servers.xml where the
+     * key is the name of the configuration, server name="NSCA">, the value is
+     * the class. The class must implement the Server class. The serverSet is
+     * provided through the ConfigurationManager.
      */
-    private Map<String,Class<?>> serverSet = new HashMap<String,Class<?>>();
+    private Map<String, Class<?>> serverSet = new HashMap<String, Class<?>>();
     private static final String GETINSTANCE = "getInstance";
     private static final String UNREGISTER = "unregister";
 
@@ -61,134 +61,123 @@ public final class ServerExecutor {
         serverSet = ConfigurationManager.getInstance().getServerClassMap();
     }
 
-
     /**
      * Factory to get a ServerExecutor instance.
+     * 
      * @return
      */
     synchronized public static ServerExecutor getInstance() {
         if (serverexecutor == null) {
-            serverexecutor= new ServerExecutor();
+            serverexecutor = new ServerExecutor();
         }
         return serverexecutor;
     }
 
-
     /**
-     * Call all registered Server implementations and invoke their 
-     * unregister(name) method to remove them from the Map of their class 
-     * specific Server class.
-     * This call is used when it need to be reloaded to re-read configuration.
-     * The "name" is what its register as for the specific Server 
-     * implementation. 
+     * Call all registered Server implementations and invoke their
+     * unregister(name) method to remove them from the Map of their class
+     * specific Server class. This call is used when it need to be reloaded to
+     * re-read configuration. The "name" is what its register as for the
+     * specific Server implementation.
      */
-    synchronized public void unregisterAll() {
+    public synchronized void unregisterAll() {
         Iterator<String> iter = serverSet.keySet().iterator();
 
-        while (iter.hasNext()) {    
+        while (iter.hasNext()) {
             String name = iter.next();
-            try {    
-                Method method = serverSet.get(name).getMethod(UNREGISTER,String.class);
-                method.invoke(null,name);
-            } catch (IllegalArgumentException e) {
-                LOGGER.error(e.toString(), e);
-            } catch (IllegalAccessException e) {
-                LOGGER.error(e.toString(), e);
-            } catch (InvocationTargetException e) {
-                LOGGER.error(e.toString(), e);
-            } catch (SecurityException e) {
-                LOGGER.error(e.toString(), e);
-            } catch (NoSuchMethodException e) {
-                LOGGER.error(e.toString(), e);
+            try {
+                Method method = serverSet.get(name).getMethod(UNREGISTER,
+                        String.class);
+                method.invoke(null, name);
+            } catch (IllegalArgumentException |  
+                    IllegalAccessException | 
+                    InvocationTargetException | 
+                    SecurityException | 
+                    NoSuchMethodException e) {
+                LOGGER.error("Failed to unregister {} ", name, e);
             }
         }
         serverexecutor = null;
     }
 
     /**
-     * The execute method is called every time a ServiceJob has been execute 
-     * according to Service configured schedule.  
-     * The method iterate through the ServerSet map and retrieve each server 
-     * object and invoke the send(Service service) method define in the Server 
-     * interface for each server in the maps. 
-     * @param service the Service object that contain data to be send to the 
-     * servers.
+     * The execute method is called every time a ServiceJob has been execute
+     * according to Service configured schedule. The method iterate through the
+     * ServerSet map and retrieve each server object and invoke the send(Service
+     * service) method define in the Server interface for each server in the
+     * maps.
+     * 
+     * @param service
+     *            the Service object that contain data to be send to the
+     *            servers.
      */
-    synchronized public void execute(ServiceTO serviceTo) {
-//
-//        final Timer timer = Metrics.newTimer(ServerExecutor.class, 
-//                "execute" , TimeUnit.MILLISECONDS, TimeUnit.SECONDS);
-//        final TimerContext context = timer.time();
+    public synchronized void execute(ServiceTO serviceTo) {
 
-        final Timer timer = MetricsManager.getTimer(ServerExecutor.class,"execute");
+        final Timer timer = MetricsManager.getTimer(ServerExecutor.class,
+                "execute");
         final Timer.Context context = timer.time();
-        try { 
+        try {
             Iterator<String> iter = serverSet.keySet().iterator();
 
-            while (iter.hasNext()) {    
+            while (iter.hasNext()) {
                 String name = iter.next();
-                try {    
+                try {
 
-                    Method method = serverSet.get(name).getMethod(GETINSTANCE,String.class);
-                    Server server = (Server) method.invoke(null,name);
+                    Method method = serverSet.get(name).getMethod(GETINSTANCE,
+                            String.class);
+                    Server server = (Server) method.invoke(null, name);
 
                     server.send(serviceTo);
 
-                } catch (IllegalArgumentException e) {
-                    LOGGER.error(e.toString(), e);
-                } catch (IllegalAccessException e) {
-                    LOGGER.error(e.toString(), e);
-                } catch (InvocationTargetException e) {
-                    LOGGER.error(e.toString(), e);
-                } catch (SecurityException e) {
-                    LOGGER.error(e.toString(), e);
-                } catch (NoSuchMethodException e) {
-                    LOGGER.error(e.toString(), e);
-                } catch (ServerException e) {
-                    LOGGER.error(e.toString(), e);
+                } catch (IllegalArgumentException |
+                        IllegalAccessException |
+                        InvocationTargetException | 
+                        SecurityException | 
+                        NoSuchMethodException | 
+                        ServerException e) {
+                    LOGGER.error("Failed to execute {} ", name, e);
                 }
             }
-        }finally {          
-            Long duration = context.stop()/1000000;
+        } finally {
+            Long duration = context.stop() / MetricsManager.TO_MILLI;
             LOGGER.debug("All servers execution time: {} ms", duration);
         }
     }
 
-    synchronized public void executeInternal(String host, String service, NAGIOSSTAT level, String message) {
+    public synchronized void executeInternal(String host, String service,
+            NAGIOSSTAT level, String message) {
 
-    	final Timer timer = MetricsManager.getTimer(ServerExecutor.class,"executeInternal");
+        final Timer timer = MetricsManager.getTimer(ServerExecutor.class,
+                "executeInternal");
         final Timer.Context context = timer.time();
-        
-        try { 
+
+        try {
             Iterator<String> iter = serverSet.keySet().iterator();
 
-            while (iter.hasNext()) {    
+            while (iter.hasNext()) {
                 String name = iter.next();
-                try {    
+                try {
 
-                    Method method = serverSet.get(name).getMethod(GETINSTANCE,String.class);
+                    Method method = serverSet.get(name).getMethod(GETINSTANCE,
+                            String.class);
 
-                    Object serverobj = method.invoke(null,name);
+                    Object serverobj = method.invoke(null, name);
                     if (serverobj instanceof ServerInternal) {
-                        ((ServerInternal) serverobj).sendInternal(host, service, level, message);
+                        ((ServerInternal) serverobj).sendInternal(host,
+                                service, level, message);
                     }
-                    
-                } catch (IllegalArgumentException e) {
-                    LOGGER.error(e.toString(), e);
-                } catch (IllegalAccessException e) {
-                    LOGGER.error(e.toString(), e);
-                } catch (InvocationTargetException e) {
-                    LOGGER.error(e.toString(), e);
-                } catch (SecurityException e) {
-                    LOGGER.error(e.toString(), e);
-                } catch (NoSuchMethodException e) {
-                    LOGGER.error(e.toString(), e);
+
+                } catch (IllegalArgumentException | 
+                        IllegalAccessException | 
+                        InvocationTargetException | 
+                        SecurityException | 
+                        NoSuchMethodException e) {
+                    LOGGER.error("Failed to execute internal {} ", name, e);
                 }
             }
-        }finally {          
-            Long duration = context.stop()/1000000;
+        } finally {
+            Long duration = context.stop() / MetricsManager.TO_MILLI;
             LOGGER.debug("All servers execution time: {} ms", duration);
         }
     }
 }
-
