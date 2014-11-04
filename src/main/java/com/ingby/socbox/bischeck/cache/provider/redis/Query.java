659907706019af3a8c1fded85ae4acd87bd5fd3d
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2014-11-04 22:26:13 +0000

    Remove deprecated method

--- src/main/java/com/ingby/socbox/bischeck/cache/provider/redis/Query.java
+++ src/main/java/com/ingby/socbox/bischeck/cache/provider/redis/Query.java
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-*/
+ */
 
 package com.ingby.socbox.bischeck.cache.provider.redis;
 
@@ -27,149 +27,111 @@ import org.slf4j.LoggerFactory;
 import com.ingby.socbox.bischeck.cache.LastStatus;
 
 /**
- * Different static query methods to search a {@link LinkedList of {@link LastStatus} objects.
- *
+ * Different static query methods to search a {@link LinkedList of
+ * {@link LastStatus} objects.
+ * 
  */
 public class Query {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(Query.class);
 
-    
+    private Query() {
+
+    }
+
     /**
-     * The method search for the LastStatus object stored in the cache that has 
+     * The method search for the LastStatus object stored in the cache that has
      * a timestamp closest to the time parameter.
-     * @param time the timestamp to search for
-     * @param listtosearch the list to search in 
+     * 
+     * @param time
+     *            the timestamp to search for
+     * @param listtosearch
+     *            the list to search in
      * @return the {@link LastStatus} object closes to the time
      */
-    public static LastStatus nearest(long time, LinkedList<LastStatus> listtosearch) {
-        
+    public static LastStatus nearest(long time,
+            LinkedList<LastStatus> listtosearch) {
+
         LOGGER.debug("Find value in cache at {}", new java.util.Date(time));
-        
-        if (time > listtosearch.getFirst().getTimestamp() || 
-            time < listtosearch.getLast().getTimestamp() ) {
+
+        if (time > listtosearch.getFirst().getTimestamp()
+                || time < listtosearch.getLast().getTimestamp()) {
             return null;
         }
-        
+
         LastStatus nearest = null;
         long bestDistanceFoundYet = Long.MAX_VALUE;
-        
+
         for (int i = 0; i < listtosearch.size(); i++) {
             long d1 = Math.abs(time - listtosearch.get(i).getTimestamp());
             long d2;
-            if (i+1 < listtosearch.size()) {
-                d2 = Math.abs(time - listtosearch.get(i+1).getTimestamp());
-            } else { 
+            if (i + 1 < listtosearch.size()) {
+                d2 = Math.abs(time - listtosearch.get(i + 1).getTimestamp());
+            } else {
                 d2 = Long.MAX_VALUE;
             }
-            
-            if ( d1 < bestDistanceFoundYet ) {
 
-                // For the moment, this value is the nearest to the desired number...
+            if (d1 < bestDistanceFoundYet) {
+
+                // For the moment, this value is the nearest to the desired
+                // number...
                 bestDistanceFoundYet = d1;
                 nearest = listtosearch.get(i);
-                if (d1 <= d2) { 
+                if (d1 <= d2) {
                     LOGGER.debug("Break at index {}", i);
                     break;
                 }
             }
         }
-        
+
         return nearest;
     }
-    
-    
+
     /**
-     * Return the cache index closes to a timestamp 
-     * @param time the timestamp to search the closest index for
-     * @param listtosearch the list to search in
+     * Return the cache index closes to a timestamp
+     * 
+     * @param time
+     *            the timestamp to search the closest index for
+     * @param listtosearch
+     *            the list to search in
      * @return cache the index closest to the timestamp
      */
-    public static Integer nearestByIndex(long time, LinkedList<LastStatus> listtosearch) {
-        
+    public static Integer nearestByIndex(long time,
+            LinkedList<LastStatus> listtosearch) {
+
         LOGGER.debug("Find value in cache at {}", new java.util.Date(time));
-        
-        if (time > listtosearch.getFirst().getTimestamp() || 
-            time < listtosearch.getLast().getTimestamp() ) {
+
+        if (time > listtosearch.getFirst().getTimestamp()
+                || time < listtosearch.getLast().getTimestamp()) {
             return null;
         }
-        
+
         Integer index = null;
         long bestDistanceFoundYet = Long.MAX_VALUE;
         // We iterate on the array...
         for (int i = 0; i < listtosearch.size(); i++) {
             long d1 = Math.abs(time - listtosearch.get(i).getTimestamp());
             long d2;
-            if (i+1 < listtosearch.size()) {
-                d2 = Math.abs(time - listtosearch.get(i+1).getTimestamp());
-            } else { 
+            if (i + 1 < listtosearch.size()) {
+                d2 = Math.abs(time - listtosearch.get(i + 1).getTimestamp());
+            } else {
                 d2 = Long.MAX_VALUE;
             }
-            
-            if ( d1 < bestDistanceFoundYet ) {
 
-                // For the moment, this value is the nearest to the desired number...
+            if (d1 < bestDistanceFoundYet) {
+
+                // For the moment, this value is the nearest to the desired
+                // number...
                 bestDistanceFoundYet = d1;
-                index=i;
-                if (d1 <= d2) { 
+                index = i;
+                if (d1 <= d2) {
                     LOGGER.debug("Break at index {}", i);
                     break;
                 }
             }
         }
-        
+
         return index;
-    }
-
-    /**
-     * Search for all LastStatus elements in the cache that exists between the 
-     * time fromtime and to totime.
-     * Following checks are done:</br>
-     * check if totime < fromtime</br>
-     * check if first elments timestamp is 
-     * @param to the to timestamp
-     * @param from the from timestamp
-     * @param listtosearch the list to search in
-     * @return a copy of list and its elements
-     * @deprecated 
-     */
-    static public LinkedList<LastStatus> findByListToFrom(long fromtime, long totime, LinkedList<LastStatus> listtosearch){
-        LinkedList<LastStatus> list = new LinkedList<LastStatus>();
-
-        Integer toindex = null;
-        Integer fromindex = null;
-        
-        
-        if (totime < fromtime) {
-            return null;
-        }
-        
-        // if outside take all 
-        
-        if (totime > listtosearch.getFirst().getTimestamp() &&
-                fromtime < listtosearch.getLast().getTimestamp() ) {
-            list.addAll(listtosearch);
-            return list;
-        } else if (totime > listtosearch.getFirst().getTimestamp()) {
-            toindex=0;
-        } else if (fromtime < listtosearch.getLast().getTimestamp()) {
-            fromindex=listtosearch.size()-1;
-        }
-        
-        if (toindex == null) {
-            toindex = Query.nearestByIndex(totime, listtosearch);
-        }
-        if (fromindex == null) {
-            fromindex = Query.nearestByIndex(fromtime, listtosearch);
-        }
-        
-        LOGGER.debug("fromindex: {} toindex: {}", fromindex, toindex);
-        for (int i = fromindex; i>toindex-1; i--){
-            LastStatus ls = listtosearch.get(i).copy();
-            list.addFirst(ls);
-        }
-        
-        return list;
     }
 
 }
