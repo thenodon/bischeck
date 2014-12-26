f0ab489599ee8423703d37efe14ee8a80f55749f
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2014-12-26 09:11:03 +0000

    Add exception handling for illegal formated servicedef, like missing ] and empty []. Add unit test

--- src/main/java/com/ingby/socbox/bischeck/ServiceDef.java
+++ src/main/java/com/ingby/socbox/bischeck/ServiceDef.java
@@ -15,97 +15,130 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-*/
+ */
 package com.ingby.socbox.bischeck;
 
 import java.util.StringTokenizer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * The class manage the parsing of a host-service-serviceitem string and 
- * breaks it up in host, service and serviceitem. The string can also take the
- * string host-service-serviceitem[indexstring]. If the string include index the
- * method hasIndex return true and the method getIndex return the index string 
- * part.
+ * The class manage the parsing of a host-service-serviceitem string and breaks
+ * it up in host, service and serviceitem. The string can also take the string
+ * host-service-serviceitem[indexstring]. If the string include index the method
+ * hasIndex return true and the method getIndex return the index string part.
  * 
  */
 public class ServiceDef {
+
+    private final static Logger LOGGER = LoggerFactory
+            .getLogger(ServiceDef.class);
 
     private String hostName = null;
     private String serviceName = null;
     private String serviceItemName = null;
     private Boolean hasIndex = false;
     private String indexstr = null;
-    
+
     /**
-     * Constructor 
-     * @param servicedef in the format of host-service-serviceitem[X] where X
-     * can be single index, time, index range or time range. If the any part of 
-     * the host, service or serviceitem name include a dash (-) it must be 
-     * quoted.
+     * Constructor
+     * 
+     * @param servicedef
+     *            in the format of host-service-serviceitem[X] where X can be
+     *            single index, time, index range or time range. If the any part
+     *            of the host, service or serviceitem name include a dash (-) it
+     *            must be quoted.
      */
     public ServiceDef(String servicedef) {
-        
+
         int indexstart = servicedef.indexOf('[');
         int indexend;
-        
+
         if (indexstart != -1) {
+
             indexend = servicedef.indexOf(']');
-            indexstr = servicedef.substring(indexstart+1, indexend);
+            if (indexend == -1) {
+                LOGGER.error(
+                        "Invalid format for service definition {}, missing ]",
+                        servicedef);
+                throw new IllegalArgumentException(
+                        "Invalid format for service definition " + servicedef
+                                + ", missing ]");
+            }
+
+            indexstr = servicedef.substring(indexstart + 1, indexend);
+            if (indexstr.isEmpty()) {
+                LOGGER.error(
+                        "Invalid format for service definition {}, empty index.",
+                        servicedef);
+                throw new IllegalArgumentException(
+                        "Invalid format for service definition " + servicedef
+                                + ", empty index.");
+
+            }
+
             servicedef = servicedef.substring(0, indexstart);
             hasIndex = true;
         }
-        
-        String servicedefQuoted = servicedef.replaceAll(ObjectDefinitions.getCacheQuoteString(), ObjectDefinitions.getQuoteConversionString());
-        StringTokenizer token = new StringTokenizer(servicedefQuoted,ObjectDefinitions.getCacheKeySep());
 
-        hostName = ((String) token.nextToken()).
-                replaceAll(ObjectDefinitions.getQuoteConversionString(), ObjectDefinitions.getCacheKeySep());
-        serviceName = (String) token.nextToken().
-                replaceAll(ObjectDefinitions.getQuoteConversionString(), ObjectDefinitions.getCacheKeySep());
-        serviceItemName = (String) token.nextToken().
-                replaceAll(ObjectDefinitions.getQuoteConversionString(), ObjectDefinitions.getCacheKeySep());        
+        String servicedefQuoted = servicedef.replaceAll(
+                ObjectDefinitions.getCacheQuoteString(),
+                ObjectDefinitions.getQuoteConversionString());
+        StringTokenizer token = new StringTokenizer(servicedefQuoted,
+                ObjectDefinitions.getCacheKeySep());
+
+        hostName = ((String) token.nextToken()).replaceAll(
+                ObjectDefinitions.getQuoteConversionString(),
+                ObjectDefinitions.getCacheKeySep());
+        serviceName = (String) token.nextToken().replaceAll(
+                ObjectDefinitions.getQuoteConversionString(),
+                ObjectDefinitions.getCacheKeySep());
+        serviceItemName = (String) token.nextToken().replaceAll(
+                ObjectDefinitions.getQuoteConversionString(),
+                ObjectDefinitions.getCacheKeySep());
 
     }
 
-    
     /**
      * Indicator if index is provided
+     * 
      * @return true if a index is provided
      */
     public Boolean hasIndex() {
         return hasIndex;
     }
-    
+
     /**
-     * Get the index part of the service definition without the square 
-     * bracket.
+     * Get the index part of the service definition without the square bracket.
+     * 
      * @return
      */
     public String getIndex() {
         return indexstr;
     }
-    
-    
+
     /**
      * Get the host name of the service definition
+     * 
      * @return
      */
     public String getHostName() {
         return hostName;
     }
-    
 
     /**
      * Get the service name of the service definition
+     * 
      * @return
      */
     public String getServiceName() {
         return serviceName;
     }
-    
-    
+
     /**
      * Get the serviceitem name of the service definition
+     * 
      * @return
      */
     public String getServiceItemName() {
